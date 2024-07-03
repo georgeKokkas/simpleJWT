7877eb65aa0b7af0762765d07d61d146534dfbba
georgeKokkas <geokokkas87@gmail.com>
2024-07-03 15:56:09 +0300

    Refresh & Rotating Tokens

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -10,6 +10,7 @@ export const AuthProvider = ({children}) => {
 
     const [authTokens, setAuthTokens] = useState( () => localStorage.getItem('authTokens') ? JSON.parse(localStorage.getItem('authTokens')) : null)
     const [user, setUser] = useState( () => localStorage.getItem('authTokens') ? jwtDecode(localStorage.getItem('authTokens')) : null)
+    const [loading, setLoading] = useState(true)
 
     const navigate = useNavigate()
 
@@ -40,11 +41,41 @@ export const AuthProvider = ({children}) => {
         navigate('/login')
     }
 
+    const updateToken = async () => {
+        console.log('Update token called!')
+        const response = await fetch('http://127.0.0.1:8000/api/token/refresh/', {
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify({'refresh':authTokens.refresh})
+        })
+        const data = await response.json();
+        if (response.status === 200){
+            setAuthTokens(data)
+            setUser(jwtDecode(data.access))
+            localStorage.setItem('authTokens', JSON.stringify(data))
+        } else {
+            logoutUser()
+        }
+    }
+
     const contextData = {
         user:user,
         loginUser:loginUser,
         logoutUser:logoutUser,
     }
+
+    useEffect(() => {
+
+        const interval = setInterval(()=> {
+            if(authTokens){
+                updateToken()
+            }
+        }, 2000)
+        return ()=> clearInterval(interval)
+
+    }, [authTokens, loading])
 
     return(
         <AuthContext.Provider value={contextData} >
