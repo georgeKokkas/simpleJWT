3a6fa5fdbd19812017c5cbc17e2d43d44910cae3
georgeKokkas <geokokkas87@gmail.com>
2024-07-03 17:49:26 +0300

    Logout User on Fail

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -3,7 +3,7 @@ import AuthContext from '../context/AuthContext'
 
 const HomePage = () => {
   let [notes, setNotes] = useState([])
-  let {authTokens} = useContext(AuthContext)
+  let {authTokens, logoutUser} = useContext(AuthContext)
 
   useEffect(()=> {
       getNotes()
@@ -18,7 +18,12 @@ const HomePage = () => {
       }
     })
     let data = await response.json()
-    setNotes(data)
+
+    if(response.status === 200){
+      setNotes(data)
+    } else if(response.statusText === 'Unauthorized'){
+      logoutUser()
+    }
   }
 
   return (
